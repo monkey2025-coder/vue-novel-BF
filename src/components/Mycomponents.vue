02d1433629d2a796b0d1d20c6dbcad69198a188c
chenziying1 <1060324818@qq.com>
2026-04-03 16:34:26 +0800

    feat:axios post与get 示例

--- src/components/Mycomponents.vue
+++ src/components/Mycomponents.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <div>{{ data }}</div>
+    <button @click="addUser">点击提交数据</button>
+    <button @click="getData">点击获取数据</button>
   </div>
 </template>
 
@@ -17,10 +19,31 @@ export default {
   methods: {
     async getData() {
       try {
-        const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        const res = await axios.get('https://jsonplaceholder.typicode.com/users')
         this.data = res.data
       } catch (err) {
         console.error('请求失败', err)
+      }
+    },
+    async addUser() {
+      this.loading = true
+      this.error = ''
+
+      try {
+        const params = {name: 'Vue3 用户',email: 'test@test.com'}
+
+        const res = await axios.post(
+          'https://jsonplaceholder.typicode.com/users',
+          params
+        )
+
+        console.log('POST 请求成功：', res.data)
+        alert('添加成功！返回ID：' + res.data.id)
+      } catch (err) {
+        this.error = '添加失败：' + err.message
+        console.error('POST 请求错误：', err)
+      } finally {
+        this.loading = false
       }
     }
   },

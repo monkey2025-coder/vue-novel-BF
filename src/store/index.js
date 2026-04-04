547fbc207f02dd051cf3498e5b50e4d1de686eeb
“ <1060324818@qq.com>
2026-04-04 12:57:49 +0800

    feat:vue状态管理1（储存全局数据）

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore } from 'vuex'
+
+// 直接导出，甚至都不用写了
+export default createStore({
+    state: {
+        count: 0
+    }
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,18 @@
 <template> 
     <div>
         <h1>Home</h1>
+        <p>当前计数: {{ $store.state.count }}</p>
+        <p>当前计数: {{ count }}</p>
     </div>
 </template>
+
+
+<script>
+import {mapState} from 'vuex'
+
+export default {
+
+    // 专门读取store中的数据
+    computed: mapState(['count'])
+}
+</script>

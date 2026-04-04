f2d6479b7dc2632ced16b4d9c1fa592c342120a3
“ <1060324818@qq.com>
2026-04-04 13:48:55 +0800

    feat:vuex核心

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,30 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 // 直接导出，甚至都不用写了
 export default createStore({
     state: {
         count: 0
+    },
+    getters: {
+        getCount(state) {
+            return state.count > 0 ? state.count : "count is negative"
+        }
+    },
+    mutations: {
+        increment(state) {
+            state.count++
+        },
+        increment_num(state, num) {
+            state.count += num
+        }
+    },
+    actions: {
+        asyncincrement_num({commit}) {
+            axios.get('/api').then(res => {
+                commit('increment_num', res.status)
+            })
+        }
     }
+
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,43 @@
         <h1>Home</h1>
         <p>当前计数: {{ $store.state.count }}</p>
         <p>当前计数: {{ count }}</p>
+        <p>当前计数: {{ $store.getters.getCount }}</p>
+        <p>当前计数: {{ getCount }}</p>
+        <button @click="increment">增加</button>
+        <button @click="increment2">自定义增加</button>
+        <button @click="increment3">自定义增加2</button>
+        <button @click="increment4">异步增加</button>
     </div>
 </template>
 
 
 <script>
-import {mapState} from 'vuex'
+import {mapState, mapGetters, mapMutations} from 'vuex'
 
 export default {
 
     // 专门读取store中的数据
-    computed: mapState(['count'])
+    computed: {
+        ...mapState(['count']),
+        ...mapGetters(['getCount'])
+    },
+    methods: {
+        ...mapMutations(['increment', 'increment_num']),
+        // 自定义方法
+        increment2() {
+            this.$store.commit('increment')
+        },
+        // 自定义方法2
+        increment3() {
+            // this.$store.commit('increment_num', 2)
+            this.increment_num(2)
+        },
+        // 自定义方法3
+        increment4() {
+            this.$store.dispatch('asyncincrement_num')
+        }
+
+        
+    }
 }
 </script>
